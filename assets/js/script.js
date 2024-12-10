4d6bbc288f6ebbeb64bdd8b370d1c75a2dd448b0
Giuseppe Davide Salvatore Pomo <giuseppepomo8@gmail.com>
2024-12-10 17:39:51 +0100

    terzo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,51 @@
 //-------Primo esercizio------//
 
-
 //globali
-const savedValueElement = document.getElementById('savedValue');
-const nameInput = document.getElementById('nameInput');
-const saveBtn = document.getElementById('saveBtn');
-const removeBtn = document.getElementById('removeBtn');
+const savedValueElement = document.getElementById("savedValue");
+const nameInput = document.getElementById("nameInput");
+const saveBtn = document.getElementById("saveBtn");
+const removeBtn = document.getElementById("removeBtn");
 
-
-//reset 
+//reset
 function resetOnPageLoad() {
-    localStorage.clear(); 
-    updateSavedValue();  
-    nameInput.value = '';
-  }
+  localStorage.clear();
+  updateSavedValue();
+  nameInput.value = "";
+}
 
-  //aggiunge valore
+//aggiunge valore
 function updateSavedValue() {
-    const savedName = localStorage.getItem('name');
-    savedValueElement.textContent = savedName ? `Valore salvato: ${savedName}` : 'Nessun valore salvato';
+  const savedName = localStorage.getItem("name");
+  savedValueElement.textContent = savedName
+    ? `Valore salvato: ${savedName}`
+    : "Nessun valore salvato";
+}
+
+saveBtn.addEventListener("click", () => {
+  const name = nameInput.value.trim();
+  if (name) {
+    localStorage.setItem("name", name);
+    updateSavedValue();
+  } else {
+    alert("Per favore, inserisci un nome");
   }
-  
-
-  saveBtn.addEventListener('click', () => {
-    const name = nameInput.value.trim();
-    if (name) {
-      localStorage.setItem('name', name);
-      updateSavedValue();
-    } else {
-      alert('Per favore, inserisci un nome!');
-    }
-  });
-
+});
 
 //btn rimuovi
-  removeBtn.addEventListener('click', () => {
-    localStorage.removeItem('name');
-    updateSavedValue();
-  });
+removeBtn.addEventListener("click", () => {
+  localStorage.removeItem("name");
+  updateSavedValue();
+});
 
+//--------Secondo Esercizio------//
 
+const counterElement = document.getElementById("counter");
 
-  //--------Secondo Esercizio------//
+let counter = parseInt(sessionStorage.getItem("counter")) || 0;
+counterElement.textContent = counter;
+
+setInterval(() => {
+  counter++;
+  counterElement.textContent = counter;
+  sessionStorage.setItem("counter", counter);
+}, 1000);
